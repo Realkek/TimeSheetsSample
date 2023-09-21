45b3c0986bca9c69f024b54e2619987b90008477
Mikhail Turlyakov <reelkek@gmail.com>
2023-09-21 17:21:14 +0400

    add login responce model

---
+++ Models/DataTransferObjects/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TImeSheetsSample.Models.DataTransferObjects
+{
+    /// <summary> Информация об аутентификации пользователя </summary>
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public long ExpiresIn { get; set; }
+    }
+}
