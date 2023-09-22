fa964031b8a37db914168dfdc874124e0e9c9a00
Mikhail Turlyakov <reelkek@gmail.com>
2023-09-22 12:01:58 +0400

    add LoginController

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TImeSheetsSample.Domain.Services.Interfaces;
+using TImeSheetsSample.Domain.ValueObjects;
+using TImeSheetsSample.Models.DataTransferObjects;
+
+namespace TImeSheetsSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController: ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService loginService, IUserService userService)
+        {
+            _loginService = loginService;
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var user = await _userService.GetUser(request);
+
+            var loginResponse = await _loginService.Authenticate(user);
+
+            return Ok(loginResponse);
+        }
+        
+        [HttpGet("refresh")]
+        public async Task<IActionResult> Refresh()
+        {
+            // return new (refresh, access);
+            
+            var rub100 = Money.FromDecimal(100);
+
+            return null;
+        }
+    }
+}
