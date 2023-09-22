8eefbe95a57e8fac9c0626085d9f73f628dbe27c
Mikhail Turlyakov <reelkek@gmail.com>
2023-09-22 12:01:38 +0400

    add constants folder; add UserRolesConstants, ValidationMessagesConstants

---
+++ Infrastructure/Constants/UserRolesConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace TImeSheetsSample.Infrastructure.Constants
+{
+    public static class UserRolesConstants
+    {
+        public static string User = "user";
+        public static string Admin = "admin";
+        public static string Client = "client";
+    }
+}
---
+++ Infrastructure/Constants/ValidationMessagesConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace TImeSheetsSample.Infrastructure.Constants
+{
+    public static class ValidationMessagesConstants
+    {
+        public const string SheetAmount = "Amount should be between 0 and 8 hours.";
+        public const string InvalidValue = "Incorrect value";
+    }
+}
