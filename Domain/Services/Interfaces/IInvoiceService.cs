84649f349cefcd19a57f0467181a76a599889b23
Mikhail Turlyakov <reelkek@gmail.com>
2023-09-22 13:23:26 +0400

    add interface for Invoice service

---
+++ Domain/Services/Interfaces/IInvoiceService.cs
@@ -0,0 +1,9 @@
+﻿using TImeSheetsSample.Models.DataTransferObjects;
+
+namespace TImeSheetsSample.Domain.Services.Interfaces
+{
+    public interface IInvoiceService
+    {
+        Task<Guid> Create(InvoiceRequest request);
+    }
+}
