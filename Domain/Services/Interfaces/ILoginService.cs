8e765a165025366ce125ffad8a55b2dc98c9daac
Mikhail Turlyakov <reelkek@gmail.com>
2023-09-22 13:21:11 +0400

    add interface for login service

---
+++ Domain/Services/Interfaces/ILoginService.cs
@@ -0,0 +1,10 @@
+﻿using TImeSheetsSample.Models.DataTransferObjects;
+using TImeSheetsSample.Models.Entities;
+
+namespace TImeSheetsSample.Domain.Services.Interfaces
+{
+    public interface ILoginService
+    {
+        Task<LoginResponse> Authenticate(User user);
+    }
+}
