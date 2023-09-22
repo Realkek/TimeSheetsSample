d6b309e1d233127ed1d322e5203ae8cbe367b645
Mikhail Turlyakov <reelkek@gmail.com>
2023-09-22 13:16:39 +0400

    move sheet service to Services

---
+++ Domain/Services/Implementation/SheetService.cs
@@ -0,0 +1,52 @@
+﻿using TImeSheetsSample.Data_Layer.Interfaces;
+using TImeSheetsSample.Domain.Aggregates.SheetAggregate;
+using TImeSheetsSample.Domain.Services.Interfaces;
+using TImeSheetsSample.Models.DataTransferObjects;
+using TImeSheetsSample.Models.Entities;
+
+namespace TImeSheetsSample.Domain.Services.Implementation;
+
+/// <summary>
+/// Сервис оперирования рабочими табелями
+/// </summary>
+public class SheetService : ISheetService
+{
+    private readonly ISheetRepo _sheetRepo;
+    private readonly ISheetAggregateRepo _sheetAggregateRepo;
+
+    public SheetService(ISheetRepo sheetRepo)
+    {
+        _sheetRepo = sheetRepo;
+    }
+
+    public async Task<Sheet> GetItem(Guid id)
+    {
+        return await _sheetRepo.GetItem(id);
+    }
+
+    public async Task<IEnumerable<Sheet>> GetItems()
+    {
+        return await _sheetRepo.GetItems();
+    }
+
+    public async Task<Guid> Create(SheetRequest sheetRequest)
+    {
+        var sheet = SheetAggregate.CreateFromSheetRequest(sheetRequest);
+
+        await _sheetRepo.Add(sheet);
+        return sheet.Id;
+    }
+
+    public async Task Approve(Guid sheetId)
+    {
+        var sheet = await _sheetAggregateRepo.GetItem(sheetId);
+        sheet.ApproveSheet();
+        await _sheetAggregateRepo.Update(sheet);
+    }
+
+    public async Task Update(Guid id, SheetRequest sheetRequest)
+    {
+        var sheet = SheetAggregate.CreateFromSheetRequest(sheetRequest);
+        await _sheetRepo.Update(sheet);
+    }
+}
