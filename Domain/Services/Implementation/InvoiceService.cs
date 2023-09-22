8a8ebb4da90b91ba58cb9d934547af5c7ef13043
Mikhail Turlyakov <reelkek@gmail.com>
2023-09-22 13:17:05 +0400

    add InvoiceService

---
+++ Domain/Services/Implementation/InvoiceService.cs
@@ -0,0 +1,34 @@
+﻿using TImeSheetsSample.Data_Layer.Interfaces;
+using TImeSheetsSample.Domain.Aggregates.InvoiceAggregate;
+using TImeSheetsSample.Domain.Services.Interfaces;
+using TImeSheetsSample.Models.DataTransferObjects;
+
+namespace TImeSheetsSample.Domain.Services.Implementation
+{
+    public class InvoiceService: IInvoiceService
+    {
+        private readonly IInvoiceRepo _invoiceRepo;
+        //private readonly ISheetRepo _sheetRepo;
+        private readonly IInvoiceAggregateRepo _invoiceAggregateRepo;
+
+        public InvoiceService(IInvoiceRepo invoiceRepo, ISheetRepo sheetRepo)
+        {
+            _invoiceRepo = invoiceRepo;
+            //_sheetRepo = sheetRepo;
+        }
+
+        public async Task<Guid> Create(InvoiceRequest request)
+        {
+            var invoice = InvoiceAggregate.Create(request.ContractId, request.DateEnd, request.DateStart);
+
+            var sheetsToInclude = await _invoiceAggregateRepo
+                .GetSheets(request.ContractId, request.DateStart, request.DateEnd);
+            
+            invoice.IncludeSheets(sheetsToInclude);
+            await _invoiceRepo.Add(invoice);
+
+            return invoice.Id;
+        }
+        
+    }
+}
